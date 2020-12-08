5d6952c56df1d6a0f3d8c428c3b64b1eb99709ac
wojtek <wojtekgrelewicz@gmail.com>
2020-12-08 11:40:42 +0100

    dodanie loomboka

--- src/main/java/com/example/User.java
+++ src/main/java/com/example/User.java
@@ -1,10 +1,15 @@
 package com.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Setter
+@Getter
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
     @Id
@@ -24,27 +29,4 @@ public class User {
     public User() {
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
